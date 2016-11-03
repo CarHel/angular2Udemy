d53342424f1bdce9c08d60794347db433157f477
Helder Carvalho <h.carvalho@thurnherr.ch>
2016-11-03 13:29:20 +0100

    .

--- 02-directives/src/app/app.component.ts
+++ 02-directives/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+private switch=true;
+private items=[1,2,3,4,5];
+private value=11;
+onSwitch()
+{
+  this.switch=!this.switch;
+}
   
 }
--- 02-directives/src/app/highlight.directive.ts
+++ 02-directives/src/app/highlight.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer,HostListener,HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
+  // constructor(private elementRef:ElementRef, private renderer:Renderer) {     
+  //       // this.elementRef.nativeElement.style.backgroundColor ='green';
+  //       // this.elementRef.nativeElement.style.foreground = 'white';
+  //        renderer.setElementStyle(elementRef.nativeElement,'background-color','red')
+  // }
 
-  constructor() { }
+  @HostListener('mouseenter') mouseover(){
+    this.backgroundColor=this.highlightColor;
+  };
+    @HostListener('mouseleave') mouseleave(){
+    this.backgroundColor=this.defaultColor;
+  };
+
+      @HostListener('click',['$event']) onmouseclick(event){
+        console.log(event)
+    this.backgroundColor='red';
+  };
+  @HostBinding('style.backgroundColor') get setColor()
+  {
+    return this.backgroundColor;
+  };
+  @Input() defaultColor='white';
+  @Input('dirHighlight') highlightColor:String='green';
+  private backgroundColor:String=this.defaultColor;
+    constructor() {     
+
+  }
 
 }
