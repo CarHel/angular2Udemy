b2cac0b3015f2ad9953d734b1e0f858fafe1c223
Helder Carvalho <h.carvalho@thurnherr.ch>
2016-11-29 08:25:37 +0100

    .

--- forms/src/app/template-driven/template-driven.component.ts
+++ forms/src/app/template-driven/template-driven.component.ts
@@ -1,12 +1,28 @@
+import { async } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+  templateUrl: 'template-driven.component.html',
+  styles:[`
+  input.ng-invalid.ng-touched{
+    border: 1px solid red
+  }
+  `]
 })
 export class TemplateDrivenComponent {
+user={
+  username:'Max',
+  email:'asda@async.com',
+  password:'asd',
+  gender:'male'
+}
+
+genders = ['male',
+'female']
+
   onSubmit(form:NgForm){
-    console.log(form);
+    console.log(form.value);
   }
 }
