cade3274e7c3820a451007b1f5dd4ae93cab279a
Helder Carvalho <h.carvalho@thurnherr.ch>
2016-11-29 07:09:58 +0100

    .

--- Routing/src/app/app.routing.ts
+++ Routing/src/app/app.routing.ts
@@ -1,14 +1,15 @@
+import { UserEditGuard } from './user/user-edit.guard';
 import { USER_ROUTES } from './user/user.routes';
 import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home-component.component';
 import { Routes, RouterModule } from '@angular/router';
 const APP_ROUTES: Routes = [
-    { path: 'user', redirectTo: 'user/1', pathMatch: 'full' },
+    { path: 'user', redirectTo: 'user/1' , pathMatch: 'full' },
      { path: 'user/', redirectTo: 'user/1', pathMatch: 'full' },
     { path: 'user/:id', component: UserComponent },
     { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
     { path: '', component: HomeComponent },
-    { path: '**', redirectTo: 'user/1', pathMatch: 'full' }
+    { path: '**', redirectTo: 'user/1', pathMatch: 'full'}
 ]
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
