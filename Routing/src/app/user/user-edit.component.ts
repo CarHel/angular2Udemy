b71a27e66478fc84e076f698c2cbb7f886ebb739
Helder Carvalho <h.carvalho@thurnherr.ch>
2016-12-15 10:53:04 +0100

    .

--- Routing/src/app/user/user-edit.component.ts
+++ Routing/src/app/user/user-edit.component.ts
@@ -1,21 +1,21 @@
 import { Observable } from 'rxjs/Rx';
 import { ComponentCanDeactivate } from './user-edit.guard';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user-edit',
     template: `
         <h3>User Edit</h3>
-        <button (click)="done = true">Done</button>
+        <button [routerLink]="['../detail']">Done</button>
         <button class="btn btn-alert" (click)="onNavigate()">Go Home</button>
     `
 })
 export class UserEditComponent implements ComponentCanDeactivate {
     done:boolean=false;
-    constructor(private router: Router){}
+    constructor(private router: Router, private activatedRouter:ActivatedRoute){}
       onNavigate() {
-    this.router.navigate(['/']);
+    this.router.navigate(['../detail'],{relativeTo:this.activatedRouter});
   }
 
   canDeactivate():Observable<boolean> | boolean{
--- Routing/src/app/user/user.routes.ts
+++ Routing/src/app/user/user.routes.ts
@@ -4,7 +4,7 @@ import { UserEditComponent } from './user-edit.component';
 import { UserDetailComponent } from './user-detail.component';
 import { Routes } from '@angular/router';
 
-export const USER_ROUTES:Routes=[
-{path:'detail',component:UserDetailComponent,canActivate:[UserDetailGuard]},
-{path:'edit',component:UserEditComponent,canDeactivate:[UserEditGuard]}
+export const USER_ROUTES: Routes = [
+    { path: 'detail', component: UserDetailComponent, canActivate: [UserDetailGuard] },    
+    { path: 'edit', component: UserEditComponent, canDeactivate: [UserEditGuard] }
 ];
